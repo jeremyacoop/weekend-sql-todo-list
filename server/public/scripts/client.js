fac0260b21e435ab3c35b35b5f42c93ba6748cfa
jeremyacoop <63517273+jeremyacoop@users.noreply.github.com>
2022-01-08 20:43:44 -0600

    Wrote client side GET

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -4,6 +4,7 @@ $(document).ready(handleReady);
 
 function handleReady() {
     console.log('jQuery is ready');
+    retrieveToDos();
     $('#todo-form').on('click', '#create-task', createToDo);
     }
 
@@ -16,3 +17,18 @@ function createToDo() {
     }
     console.log(newToDo);
 }
+
+function retrieveToDos() {
+    console.log('In retrieveToDos');
+    $.ajax({
+        type:   'GET',
+        url:    '/todolist'
+    })
+    .then(function(response) {
+        console.log(response);
+        displayToDos(response);
+    })
+    .catch(function(error) {
+        console.log('Error in GET ', error);
+    });// end GET
+}// end retrieveToDos
