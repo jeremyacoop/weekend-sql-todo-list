3a496ee419de0bd7225073f46fedea10f938f1b2
jeremyacoop <63517273+jeremyacoop@users.noreply.github.com>
2022-01-08 19:18:17 -0600

    Wrote basic server and front page setup with essential form fields

--- server/server.js
+++ server/server.js
@@ -1,12 +1,14 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const router = require('./routes/todos.router');
 
 const app = express();
 app.use(express.static('server/public'));
 app.use(bodyParser.urlencoded({extended: true}));
+app.use('/todolist', router);
 
-const PORT = 5000;
-app.listen(PORT, () => {
-    console.log('server up on port', PORT);
-    })
+const port = process.env.PORT || 5000;
+app.listen(port, () => {
+    console.log('server up on port', port);
+    });
 
